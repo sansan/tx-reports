a9afe3b1e6077ae1f0409b723bda239bd5d53761
Toms Veidemanis <toms@veidemanis.lv>
2021-12-18 00:31:36 +0200

    feat: add typings for api responses

---
+++ src/typings/api.ts
@@ -0,0 +1,60 @@
+export type ApiResponse<T> = {
+  code: '200' | string;
+  data: T;
+  error?: string | null;
+};
+
+export type Project = {
+  projectId: string;
+  userIds: string[];
+  rule: string;
+  gatewayIds: string[];
+  structure: string;
+  industry: string;
+  website: string;
+  description: string;
+  image: string;
+  name: string;
+};
+
+export type ProjectsApiResponse = ApiResponse<Project[]>;
+
+export type User = {
+  userId: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+};
+
+export type UserApiResponse = ApiResponse<User[]>;
+
+export type Gateway = {
+  gatewayId: string;
+  userIds: string[];
+  name: string;
+  type: string;
+  apiKey: string;
+  secondaryApiKey: string;
+  description: string;
+};
+
+export type GatewayApiResponse = ApiResponse<Gateway[]>;
+
+export type ReportApiRequestBody = {
+  from?: string;
+  to?: string;
+  projectId?: string;
+  gatewayId?: string;
+};
+
+export type Payment = {
+  paymentId: string;
+  amount: number;
+  projectId: string;
+  gatewayId: string;
+  userIds: string;
+  modified: string;
+  created: string;
+};
+
+export type PaymentApiResponse = ApiResponse<Payment[]>;
