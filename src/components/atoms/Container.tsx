b7edfabf74dd4314b70b3d702231ad15f9b05311
Toms Veidemanis <toms@veidemanis.lv>
2021-12-22 16:45:06 +0200

    feat: add container

---
+++ src/components/atoms/Container.tsx
@@ -0,0 +1,26 @@
+/* eslint-disable react/jsx-props-no-spreading */
+import React from 'react';
+import { Box, BoxProps } from '@chakra-ui/react';
+
+type ContainerProps = BoxProps & {
+  variant?: 'primary' | 'secondary';
+};
+
+export const Container: React.FC<ContainerProps> = ({
+  children,
+  variant = 'primary',
+  ...rest
+}) => (
+  <Box
+    {...rest}
+    bg={variant === 'primary' ? 'background.dark' : 'background.light'}
+    padding="1.25rem"
+    borderRadius="10"
+  >
+    {children}
+  </Box>
+);
+
+Container.defaultProps = { variant: 'primary' };
+
+export default Container;
