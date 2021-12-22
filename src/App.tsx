b0211a2ba5f3d3f34d9a85f3e9180d0a2d4f0862
Toms Veidemanis <toms@veidemanis.lv>
2021-12-22 16:55:22 +0200

    fixup! feat: implement redux and theme config

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import { ChakraProvider } from '@chakra-ui/react';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
+import '@fontsource/roboto';
+
+import themeConfig from 'config/theme';
+import store from 'store';
 import PagesRouter from 'pages';
 
 const App = () => (
   <BrowserRouter>
-    <ChakraProvider>
-      <PagesRouter />
+    <ChakraProvider theme={themeConfig}>
+      <Provider store={store}>
+        <PagesRouter />
+      </Provider>
     </ChakraProvider>
   </BrowserRouter>
 );
